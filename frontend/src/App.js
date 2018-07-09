c2b5d8445b6505e8c1cd2d8a122ecb03638b17b3
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2018-07-10 01:48:22 +0530

    Home: Implement a crude design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
 import Home from './pages/Home';
 import Resources from './pages/Resources';
 import Nav from './components/Nav';
 import NavDrawer from './components/NavDrawer';
 
 const drawerWidth = 240;
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: grey[800],
+    },
+  },
+});
 
 const styles = theme => ({
   root: {
@@ -27,9 +36,10 @@ const styles = theme => ({
   },
   content: {
     flexGrow: 1,
-    marginTop: 30,
+    marginTop: 55,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing.unit * 3,
+    overflow: 'scroll',
   },
 });
 
@@ -49,18 +59,20 @@ class App extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <BrowserRouter>
-        <div className={classes.root}>
-          <Nav handleDrawerToggle={() => this.drawerToggle()} />
-          <NavDrawer drawerOpen={this.state.drawerOpen} handleDrawerToggle={() => this.drawerToggle()} />
-          <div className={classes.content}>
-            <Switch>
-              <Route path={'/home/'} exact component={Home} />
-              <Route path={'/resources/'} exact component={Resources} />
-            </Switch>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div className={classes.root}>
+            <Nav handleDrawerToggle={() => this.drawerToggle()} />
+            <NavDrawer drawerOpen={this.state.drawerOpen} handleDrawerToggle={() => this.drawerToggle()} />
+            <div className={classes.content}>
+              <Switch>
+                <Route path={'/home/'} exact component={Home} />
+                <Route path={'/resources/'} exact component={Resources} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
-const Home = () => {
+const styles = theme => ({
+  title: {
+  	color: 'black',
+  },
+  flexParent: {
+  	color: 'grey',
+  	display: 'flex',
+  	alignItems: 'center',
+  },
+});
+
+const Home = (props) => {
+	const { classes } = props;
 	return (
 		<div>
-			<h1> HOME </h1>
-			<h3> Pclub Website 2.0 is here! </h3>
+			<Grid container spacing={24}>
+				<Grid item md={6} className={classes.flexParent}>
+					<img src={"/static/img/laptop.svg"} width={"95%"}  alt="Error"></img>
+				</Grid>
+				<Grid item md={6} className={classes.flexParent}>
+					<div>
+						<Typography variant="display3" className={classes.title}> We are the Pclub! </Typography>
+						<p>
+							We are a bunch of students who love coding and admire open source with all our hearts.
+							Programming club is one of the biggest clubs of Panjab University.
+							We aim to create a platform which helps students gain assistance and mentorship and eventually
+							enhance their abilities. We intend to induce and propagate enthusiasm for programming in the 
+							institute and the University. We believe that every student should have an opportunity to learn
+							how to code and we help them develop that passion for it.
+						</p>
+					</div>
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
 
-export default Home;
+export default withStyles(styles, { withTheme: true })(Home);
